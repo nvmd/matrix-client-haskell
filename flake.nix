f88f2fb695a24f6192e22c4d41b43e94e644dd3b
Sergey Kazenyuk <git.v63hf@sergey-kazenyuk.com>
2026-02-02 14:55:24 +0100

    flake: use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -6,8 +6,7 @@
   nixConfig.bash-prompt = "[nix]λ ";
 
   inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/d3780c92e64472e8f9aa54f7bbb0dd4483b98303";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
